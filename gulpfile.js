f7ebaaa3f468ded6e153d8b82cf0ef7df8e54da7
Cesar Damascena <cesar.damascena@resultate.com.br>
2017-01-06 16:06:06 -0200

    Theme base setup

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ var autoprefixer  = require( 'gulp-autoprefixer' );
 gulp.task( 'default', ['sass', 'watch'] );
 
 gulp.task('sass', function() {
-      return gulp.src('./sass/portfolio.scss')
-          .pipe(sass({style: 'compressed'}))
-          .pipe(autoprefixer('last 15 version'))
-          .pipe(gulp.dest('./css/dist/'));
+    return gulp.src('./sass/.scss')
+        .pipe(sass({style: 'compressed'}))
+        .pipe(autoprefixer('last 15 version'))
+        .pipe(gulp.dest('./css/dist/'));
 });
 
 gulp.task('watch', function () {
